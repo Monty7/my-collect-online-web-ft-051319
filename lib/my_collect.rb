760760752da733c35e0fa0990065be704cc16db5
voluble-bug-0219 <montensia1@outlook.com>
2019-05-15 22:30:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0 
+    collect = []
+    while i < array.length 
+      collect << yield(array[i])
+      i = i + 1 
+    end
+    collect
+  else
+    puts "Where's my block?"
+  end
+end
 
